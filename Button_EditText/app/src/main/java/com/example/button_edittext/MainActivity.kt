73403d9854e7108ec1a25c8e92c6b04e89037b9b
vedyaskin <k.vedyaskin@gmail.com>
2024-08-27 20:14:07 +0300

    Home_Work

---
+++ Button_EditText/app/src/main/java/com/example/button_edittext/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.button_edittext
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputText: EditText
+    private lateinit var resultText: TextView
+    private lateinit var countOfSymbols: TextView
+    private lateinit var button: Button
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        inputText = findViewById(R.id.inputText)
+        resultText = findViewById(R.id.textResult)
+        countOfSymbols = findViewById(R.id.countOfSymbols)
+        button = findViewById(R.id.button)
+
+        onButtonClick(button)
+
+    }
+    fun onButtonClick(view: View){
+        val result = inputText.text.reversed()
+        resultText.text = result
+
+        countOfSymbols.text = "Количество символов: ${result.toString().replace(" ","").length}"
+
+    }
+}
