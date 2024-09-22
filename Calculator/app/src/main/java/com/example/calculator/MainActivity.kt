da91dc71b4c2309286ffdd3e4270819d171517e7
vedyaskin <k.vedyaskin@gmail.com>
2024-09-22 17:58:39 +0300

    Калькулятор времени

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,18 +2,28 @@ package com.example.calculator
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.isDigitsOnly
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var firstTimeET: EditText
@@ -21,6 +31,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var buttonSumBTN: Button
     private lateinit var buttonDifBTN: Button
     private lateinit var resultTV: TextView
+
+    private lateinit var toolbarMain: Toolbar
+
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +45,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         firstTimeET = findViewById(R.id.timeFirstET)
         secondTimeET = findViewById(R.id.timeSecondET)
         buttonSumBTN = findViewById(R.id.SumBTN)
@@ -39,6 +53,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         resultTV = findViewById(R.id.resultTV)
         buttonDifBTN.setOnClickListener(this)
         buttonSumBTN.setOnClickListener(this)
+
+        toolbarMain = findViewById(R.id.toolBarMain)
+        setSupportActionBar(toolbarMain)
+        title = getString(R.string.time_calc)
+        toolbarMain.subtitle = "версия 1"
+        toolbarMain.setLogo(R.drawable.ic_moretime)
 
     }
 
@@ -55,6 +75,43 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             else -> 0
         }
         resultTV.text = result.toString()
+
+        val rootView = findViewById<View>(R.id.main)
+        val snackbar = Snackbar.make(rootView,"Результат: ${result}", Snackbar.LENGTH_LONG)
+        snackbar.setBackgroundTint(ContextCompat.getColor(applicationContext, R.color.white));
+        snackbar.setTextColor(Color.parseColor("#8B0000"))
+        snackbar.show()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.resetMenuMain -> {
+                firstTimeET.text.clear()
+                secondTimeET.text.clear()
+                resultTV.text = ""
+                resultTV.hint = getString(R.string.result)
+                Toast.makeText(
+                    applicationContext,
+                    "Данные очищены",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            R.id.exitMenuMain -> {
+                Toast.makeText(
+                    applicationContext,
+                    "Приложение закрыто",
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     // перевод строки в секунды
@@ -69,7 +126,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
             if (str.contains("m")) {
                 val temp = str.substring(str.indexOf("h") + 1, str.indexOf("m")).toInt()
-                if (hourOrMinEntered && temp > 60){
+                if (hourOrMinEntered && temp > 60) {
                     showAlertDialog(this, "Введено $temp мин. В часе не может быть столько.")
                 } else {
                     sec += temp * (60)
@@ -78,11 +135,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
             if (str.contains("s")) {
                 val temp = str.substring(str.indexOf("m") + 1, str.indexOf("s")).toInt()
-                if (hourOrMinEntered && temp > 60){
+
+                if (hourOrMinEntered && temp > 60) {
                     showAlertDialog(this, "Введено $temp сек. В минуте не может быть столько.")
                 } else {
                     sec += str.substring(str.indexOf("m") + 1, str.indexOf("s")).toInt()
                 }
+            }
+            if (!hourOrMinEntered) {
+                showAlertDialog(this, "$str")
             }
             return sec
 
@@ -101,7 +162,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     fun showAlertDialog(context: Context, message: String) {
-        Log.e("AlertDialog", message)
         val builder = AlertDialog.Builder(context)
         builder.setTitle("Неправильный формат времени!")
             .setMessage(message)
