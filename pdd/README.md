52c24616c35232b8c64e662967898377c12bfd34
vedyaskin <k.vedyaskin@gmail.com>
2024-09-26 23:09:09 +0300

    Правила дорожного движения

---
+++ pdd/README.md
@@ -0,0 +1,2 @@
+![image1](images/image1.png)
+![image2](images/image2.png)
---
+++ pdd/app/src/main/java/com/example/pdd/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.pdd
+
+import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var textView: TextView
+    lateinit var checkBox: CheckBox
+    lateinit var bigTextView: TextView
+    lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        textView = findViewById(R.id.textTV)
+        checkBox = findViewById(R.id.checkbox)
+        bigTextView = findViewById(R.id.pddTV)
+
+        toolbar = findViewById(R.id.toolBarMain)
+        setSupportActionBar(toolbar)
+
+        title = getString(R.string.app_name)
+
+        checkBox.setOnCheckedChangeListener {_, isChecked ->
+            if (isChecked){
+                textView.text = getString(R.string.pdd)
+                bigTextView.text = getString(R.string.big_text)
+            } else {
+                textView.text = getString(R.string.info)
+                bigTextView.text = ""
+            }
+        }
+
+
+    }
+}
