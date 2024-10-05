17f2a623f9bfdf11e5f19b20a990019199faf991
vedyaskin <k.vedyaskin@gmail.com>
2024-10-05 19:32:39 +0300

    Домашнее задание по теме "Lyfecycle"

---
+++ Android_Calculator/app/src/main/java/com/example/android_calculator/Operation.kt
@@ -0,0 +1,8 @@
+package com.example.android_calculator
+
+class Operation(private val first: Double, private val second: Double) {
+    fun sum() = first + second
+    fun dif() = first - second
+    fun mult() = first * second
+    fun div() = first / second
+}
