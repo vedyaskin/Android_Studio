48086254d723365755d18d655f04b967412312a1
krk <k.vedyaskin@gmail.com>
2024-10-24 22:50:41 +0300

    Викторина "Я историк"

--- RadioButton_HW/app/src/main/java/com/example/radiobutton_hw/ResultActivity.kt
+++ RadioButton_HW/app/src/main/java/com/example/radiobutton_hw/ResultActivity.kt
@@ -2,11 +2,9 @@ package com.example.radiobutton_hw
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.Html
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.text.HtmlCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
